edc0f395735e9c915d459ac0112b637e96e8c3b1
ricardo.rios <ricardo.rios@irrigacion.gov.ar>
2026-05-06 08:38:15 -0300

    cambios

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -15,8 +15,8 @@ const Login = () => {
     setError('');
 
     try {
-      const apiUrl = 'http://192.168.2.166:8888/damsu-app/backend/api/login.php';
-      
+      // const apiUrl = 'http://192.168.2.166:8888/damsu-app/backend/api/login.php';
+      const apiUrl = 'http://82.25.78.98/damsu-app/backend/api/login.php';
       const response = await fetch(apiUrl, {
         method: 'POST',
         headers: {
